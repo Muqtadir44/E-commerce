48414f910b0fae831de80b18f4caab32f4b9b4c9
Abdul Muqtadir <kalhoromuqtadir125@gmail.com>
2023-10-24 13:29:23 +0500

    login authentication

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -24,7 +24,8 @@
 			  	</div>
 			  	<div class="card-body">
 					<p class="login-box-msg">Sign in to start your session</p>
-					<form action="dashboard.html" method="post">
+					<form action="{{route()}}" method="post">
+                        @csrf
 				  		<div class="input-group mb-3">
 							<input type="email" class="form-control" placeholder="Email">
 							<div class="input-group-append">
