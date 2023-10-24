043ffbfc4396e8f09961f92ec4b62318e36b3d2f
Abdul Muqtadir <kalhoromuqtadir125@gmail.com>
2023-10-24 12:09:18 +0500

    admin-login

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -67,12 +67,16 @@
 		</div>
 		<!-- ./wrapper -->
 		<!-- jQuery -->
-		<script src="plugins/jquery/jquery.min.js"></script>
+        <script src="{{asset('admin-assets/plugins/jquery/jquery.min.js')}}"></script>
+		{{-- <script src="plugins/jquery/jquery.min.js"></script> --}}
 		<!-- Bootstrap 4 -->
-		<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="{{asset('admin-assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+		{{-- <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script> --}}
 		<!-- AdminLTE App -->
-		<script src="js/adminlte.min.js"></script>
+        <script src="{{asset('admin-assets/js/adminlte.min.js')}}"></script>
+		{{-- <script src="js/adminlte.min.js"></script> --}}
 		<!-- AdminLTE for demo purposes -->
-		<script src="js/demo.js"></script>
+        <script src="{{asset('admin-assets/js/demo.js')}}"></script>
+		{{-- <script src="js/demo.js"></script> --}}
 	</body>
 </html>
