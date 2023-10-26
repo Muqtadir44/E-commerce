f7f090635ea8184b8f1c6cec677cf5efe7f0967d
Abdul Muqtadir <kalhoromuqtadir125@gmail.com>
2023-10-26 13:18:17 +0500

    setting up all methods

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -7,5 +7,28 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(){
+
+    }
+
+    public function create(){
+
+    }
+
+    public function store(){
+
+    }
+
+    public function edit(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function delete(){
+
+    }
+    
 }
