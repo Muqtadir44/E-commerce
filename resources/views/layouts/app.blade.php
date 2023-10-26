9ea604836d2ded2959af0a69c42e86903f3c4136
Abdul Muqtadir <kalhoromuqtadir125@gmail.com>
2023-10-26 12:29:18 +0500

    layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>@yield('page-title')</title>
+		<!-- Google Font: Source Sans Pro -->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+		<!-- Font Awesome -->
+        <link rel="stylesheet" href="{{asset('admin-assets/plugins/fontawesome-free/css/all.min.css')}}">
+		{{-- <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css"> --}}
+		<!-- Theme style -->
+        <link rel="stylesheet" href="{{asset('admin-assets/css/adminlte.min.css')}}">
+        <link rel="stylesheet" href="{{asset('admin-assets/css/custom.css')}}">
+		{{-- <link rel="stylesheet" href="css/adminlte.min.css"> --}}
+		{{-- <link rel="stylesheet" href="css/custom.css"> --}}
+	</head>
+	<body class="hold-transition login-page">
+		@yield('content')
+		<!-- ./wrapper -->
+		<!-- jQuery -->
+        <script src="{{asset('admin-assets/plugins/jquery/jquery.min.js')}}"></script>
+		{{-- <script src="plugins/jquery/jquery.min.js"></script> --}}
+		<!-- Bootstrap 4 -->
+        <script src="{{asset('admin-assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+		{{-- <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script> --}}
+		<!-- AdminLTE App -->
+        <script src="{{asset('admin-assets/js/adminlte.min.js')}}"></script>
+		{{-- <script src="js/adminlte.min.js"></script> --}}
+		<!-- AdminLTE for demo purposes -->
+        <script src="{{asset('admin-assets/js/demo.js')}}"></script>
+		{{-- <script src="js/demo.js"></script> --}}
+	</body>
+</html>
