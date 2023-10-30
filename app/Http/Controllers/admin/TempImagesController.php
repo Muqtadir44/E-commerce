5f03184ab3661e954dc9c38e2a1545fcf1d44eba
Abdul Muqtadir <kalhoromuqtadir125@gmail.com>
2023-10-30 17:17:07 +0500

    temp image controller setting

--- app/Http/Controllers/admin/TempImagesController.php
+++ app/Http/Controllers/admin/TempImagesController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\TempImage;
 use Illuminate\Http\Request;
 
 class TempImagesController extends Controller
 {
     public function create(Request $request){
-        
+        $image = $request->image;
+        if (!empty($image)) {
+            $ext     = $image->getClientOriginalExtension();
+            $newName = time().'.'.$ext;
+
+            $tempImage = new TempImage();
+        }
     }
 }
