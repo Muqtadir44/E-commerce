0651791841f03bffa3525347fb1b87038a948e7b
Abdul Muqtadir <kalhoromuqtadir125@gmail.com>
2023-11-02 10:20:59 +0500

    setting up date formats in datatable of category

--- app/DataTables/CategoriesDataTable.php
+++ app/DataTables/CategoriesDataTable.php
@@ -45,8 +45,15 @@ class CategoriesDataTable extends DataTable
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                             </svg>';
                 }
+            })->editColumn('created_at',function($data){
+                $date = date_create($data->created_at);
+                return date_format($date,"d M-Y");
             })
-            ->rawColumns(['status','Edit'])
+            ->editColumn('updated_at',function($data){
+                $date = date_create($data->updated_at);
+                return date_format($date,"d M-Y");
+            })
+            ->rawColumns(['status','Edit','created_at','updated_at'])
             ->setRowId('id');
     }
 
