48039c6ab6f4b520365376102b9cf6588adee6e0
Muqtadir <kalhoromuqtadir125@gmail.com>
2023-11-21 12:25:17 +0500

    create sub-category

--- resources/views/admin/sub_category/create.blade.php
+++ resources/views/admin/sub_category/create.blade.php
@@ -1,13 +1,13 @@
 @extends('admin.layouts.app')
 @section('content')
-    <section class="content-header">					
+    <section class="content-header">
 					<div class="container-fluid my-2">
 						<div class="row mb-2">
 							<div class="col-sm-6">
 								<h1>Create Sub Category</h1>
 							</div>
 							<div class="col-sm-6 text-right">
-								<a href="subcategory.html" class="btn btn-primary">Back</a>
+								<a href="{{route('sub-categories.listing')}}" class="btn btn-primary">Back</a>
 							</div>
 						</div>
 					</div>
@@ -18,7 +18,7 @@
 					<!-- Default box -->
 					<div class="container-fluid">
 						<div class="card">
-							<div class="card-body">								
+							<div class="card-body">
 								<div class="row">
                                     <div class="col-md-12">
 										<div class="mb-3">
@@ -26,7 +26,7 @@
 											<select name="category" id="category" class="form-control">
                                                 @if ($categories->isNotEmpty())
                                                     @foreach ($categories as $category)
-                                                        
+
                                                     <option value="{{$category->id}}"> {{$category->name}} </option>
                                                     @endforeach
                                                 @endif
@@ -36,13 +36,13 @@
 									<div class="col-md-6">
 										<div class="mb-3">
 											<label for="name">Name</label>
-											<input type="text" name="name" id="name" class="form-control" placeholder="Name">	
+											<input type="text" name="name" id="name" class="form-control" placeholder="Name">
 										</div>
 									</div>
 									<div class="col-md-6">
 										<div class="mb-3">
 											<label for="email">Slug</label>
-											<input type="text" name="slug" id="slug" class="form-control" placeholder="Slug">	
+											<input type="text" name="slug" id="slug" class="form-control" placeholder="Slug">
 										</div>
 									</div>
                                     <div class="col-md-6">
@@ -53,13 +53,13 @@
                                                 <option value="Inactive">Inactive</option>
                                             </select>
                                         </div>
-									</div>									
+									</div>
 								</div>
-							</div>							
+							</div>
 						</div>
 						<div class="pb-5 pt-3">
 							<button class="btn btn-primary">Create</button>
-							<a href="subcategory.html" class="btn btn-outline-dark ml-3">Cancel</a>
+							<a href="{{route('sub-categories.listing')}}" class="btn btn-outline-dark ml-3">Cancel</a>
 						</div>
 					</div>
 					<!-- /.card -->
