e5f6d0adabc762f9a6e2973154018e2707f21233
Muqtadir <kalhoromuqtadir125@gmail.com>
2023-11-21 12:25:25 +0500

    sub-category listing

--- resources/views/admin/sub_category/listing.blade.php
+++ resources/views/admin/sub_category/listing.blade.php
@@ -1,4 +1,34 @@
 @extends('admin.layouts.app')
 @section('content')
-    <h1>Sub-category</h1>
+    <section class="content-header">
+        <div class="container-fluid my-2">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Sub Categories</h1>
+                </div>
+                <div class="col-sm-6 text-right">
+                    <a href="{{route('sub-categories.create')}}" class="btn btn-primary">New Sub-category</a>
+                </div>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+
+    <section class="content">
+        <!-- Default box -->
+        <div class="container-fluid">
+            @include('admin.message')
+            <div class="card">
+                <div class="card-body table-responsive">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                </div>
+            </div>
+        </div>
+        <!-- /.card -->
+        @include('admin.sub_category.edit')<!-- Update Category Modal -->
+    </section>
 @endsection
+<script src="{{asset('admin-assets/js/modal-scripts/sub-category.js')}}"></script>
