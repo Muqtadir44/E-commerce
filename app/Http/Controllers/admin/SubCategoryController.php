918205f5da3cabfa0e5da178aab232e03cb30dcb
Muqtadir <kalhoromuqtadir125@gmail.com>
2023-11-22 12:18:52 +0500

    yajra datatable link

--- app/Http/Controllers/admin/SubCategoryController.php
+++ app/Http/Controllers/admin/SubCategoryController.php
@@ -5,14 +5,20 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Models\category;
 use Illuminate\Http\Request;
-use Yajra\DataTables;
 use App\Models\SubCategory;
+use Yajra\DataTables\Facades\DataTables;
 
 class SubCategoryController extends Controller
 {
-    public function listing(){
-        
+
+    public function index(){
         return view('admin.sub_category.listing');
+    }
+    public function listing(Request $request){
+        // $sub_category = SubCategory::query()->get();
+        // return DataTables::of($sub_category)
+        // ->addIndexColumn()
+        // ->make(true);
     }
     public function create(){
         $categories = category::orderBy('name','DESC')->get();
