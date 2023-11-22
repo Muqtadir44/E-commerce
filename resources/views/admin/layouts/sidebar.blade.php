eaf19072bd2fe821041d63e30844b6fe797536bf
Muqtadir <kalhoromuqtadir125@gmail.com>
2023-11-22 12:19:20 +0500

    adding link sub-category

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -24,7 +24,7 @@
 								</a>
 							</li>
 							<li class="nav-item">
-								<a href="{{route('sub-categories.listing')}}" class="nav-link">
+								<a href="{{route('sub-categories.index')}}" class="nav-link">
 									<i class="nav-icon fas fa-file-alt"></i>
 									<p>Sub Category</p>
 								</a>
