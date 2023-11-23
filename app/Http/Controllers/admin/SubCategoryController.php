11a7ee28e90a4f1a8064dad0a8fd153c86e70394
Muqtadir <kalhoromuqtadir125@gmail.com>
2023-11-23 11:01:17 +0500

    loading yajra datatable for sub-category

--- app/Http/Controllers/admin/SubCategoryController.php
+++ app/Http/Controllers/admin/SubCategoryController.php
@@ -15,10 +15,10 @@ class SubCategoryController extends Controller
         return view('admin.sub_category.listing');
     }
     public function listing(Request $request){
-        // $sub_category = SubCategory::query()->get();
-        // return DataTables::of($sub_category)
-        // ->addIndexColumn()
-        // ->make(true);
+        $sub_category = SubCategory::query()->get();
+        return DataTables::of($sub_category)
+        ->addIndexColumn()
+        ->make(true);
     }
     public function create(){
         $categories = category::orderBy('name','DESC')->get();
