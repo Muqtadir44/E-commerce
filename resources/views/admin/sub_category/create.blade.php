52f2c8b26d542492e856893cc9f722f89780f43d
Muqtadir <kalhoromuqtadir125@gmail.com>
2023-11-24 12:40:19 +0500

    setting up form of add sub-category

--- resources/views/admin/sub_category/create.blade.php
+++ resources/views/admin/sub_category/create.blade.php
@@ -107,7 +107,7 @@
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Add Sub Category</button>
+          <button type="button" id="add-subCategory" class="btn btn-primary">Add Sub Category</button>
         </div>
       </div>
     </div>
