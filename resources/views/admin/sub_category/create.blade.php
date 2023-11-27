11747e086ebd17da87e5655c88cece0f2817621d
Muqtadir <kalhoromuqtadir125@gmail.com>
2023-11-27 10:52:22 +0500

    setting add-subcategory form

--- resources/views/admin/sub_category/create.blade.php
+++ resources/views/admin/sub_category/create.blade.php
@@ -1,72 +1,3 @@
-{{-- @extends('admin.layouts.app')
-@section('content')
-    <section class="content-header">
-					<div class="container-fluid my-2">
-						<div class="row mb-2">
-							<div class="col-sm-6">
-								<h1>Create Sub Category</h1>
-							</div>
-							<div class="col-sm-6 text-right">
-								<a href="{{route('sub-categories.listing')}}" class="btn btn-primary">Back</a>
-							</div>
-						</div>
-					</div>
-					<!-- /.container-fluid -->
-				</section>
-				<!-- Main content -->
-				<section class="content">
-					<!-- Default box -->
-					<div class="container-fluid">
-						<div class="card">
-							<div class="card-body">
-								<div class="row">
-                                    <div class="col-md-12">
-										<div class="mb-3">
-											<label for="name">Category</label>
-											<select name="category" id="category" class="form-control">
-                                                @if ($categories->isNotEmpty())
-                                                    @foreach ($categories as $category)
-
-                                                    <option value="{{$category->id}}"> {{$category->name}} </option>
-                                                    @endforeach
-                                                @endif
-                                            </select>
-										</div>
-									</div>
-									<div class="col-md-6">
-										<div class="mb-3">
-											<label for="name">Name</label>
-											<input type="text" name="name" id="name" class="form-control" placeholder="Name">
-										</div>
-									</div>
-									<div class="col-md-6">
-										<div class="mb-3">
-											<label for="email">Slug</label>
-											<input type="text" name="slug" id="slug" class="form-control" placeholder="Slug">
-										</div>
-									</div>
-                                    <div class="col-md-6">
-										<div class="mb-3">
-											<label for="email">Status</label>
-                                            <select name="status" id="" class="form-control">
-                                                <option value="Active">Active</option>
-                                                <option value="Inactive">Inactive</option>
-                                            </select>
-                                        </div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="pb-5 pt-3">
-							<button class="btn btn-primary">Create</button>
-							<a href="{{route('sub-categories.listing')}}" class="btn btn-outline-dark ml-3">Cancel</a>
-						</div>
-					</div>
-					<!-- /.card -->
-				</section>
-@endsection --}}
-
-
 <div class="modal fade" id="add-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -75,39 +6,46 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="mb-3">
-                        <label for="name">Category</label>
-                        <select class="form-control" name="" id="categories"></select>
+            <form action="">
+                @csrf
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="mb-3">
+                            <label for="name">Category</label>
+                            <select class="form-control" name="" id="categories"></select>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" id="name" class="form-control" placeholder="Name">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="email">Slug</label>
+                            <input type="text" name="slug" id="slug" class="form-control" placeholder="Slug">
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="mb-3">
+                            <label for="email">Status</label>
+                            <select name="status" id="status" class="form-control">
+                                <option value="Active">Active</option>
+                                <option value="Inactive">Inactive</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="mb-3">
+                            <button type="button" id="add-subCategory" class="btn btn-primary">Add Sub Category</button>
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="mb-3">
-                        <label for="name">Name</label>
-                        <input type="text" name="name" id="name" class="form-control" placeholder="Name">
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="mb-3">
-                        <label for="email">Slug</label>
-                        <input type="text" name="slug" id="slug" class="form-control" placeholder="Slug">
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="mb-3">
-                        <label for="email">Status</label>
-                        <select name="status" id="status" class="form-control">
-                            <option value="Active">Active</option>
-                            <option value="Inactive">Inactive</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
+            </form>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" id="add-subCategory" class="btn btn-primary">Add Sub Category</button>
         </div>
       </div>
     </div>
