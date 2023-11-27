cce5033a0a35c66578a9a892494669537ad0bd6e
Muqtadir <kalhoromuqtadir125@gmail.com>
2023-11-27 11:25:52 +0500

    sub-category add method

--- app/Http/Controllers/admin/SubCategoryController.php
+++ app/Http/Controllers/admin/SubCategoryController.php
@@ -32,6 +32,24 @@ class SubCategoryController extends Controller
         return view('admin.sub_category.create',$data);
     }
 
+    public function add(Request $request){
+        // return dd($request);
+        // return $request->all();
+        $request->validate([
+            'name'     => 'required',
+            'category' => 'required'
+        ]);
+
+        $sub_category              = new SubCategory();
+        $sub_category->category_id = $request->category;
+        $sub_category->name        = $request->name;
+        $sub_category->slug        = $request->slug;
+        $sub_category->status      = $request->notes;
+        $sub_category->save();
+
+        return response()->json($sub_category);
+    }
+
     public function get_sub_category(){
 
     }
