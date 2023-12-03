273ec1dc7db95d7f28b6e3548e24422b66587e27
Abdul Muqtadir <kalhoromuqtadir125@gmail.com>
2023-12-03 18:24:25 +0500

    sub-category edit modal

---
+++ resources/views/admin/sub_category/edit.blade.php
@@ -0,0 +1,53 @@
+<div class="modal fade" id="edit-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Sub Category</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <form action="" id="edit_sub_category_form">
+                @csrf
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="mb-3">
+                            <label for="name">Category</label>
+                            <select class="form-control" name="" id="edit_categories"></select>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" id="edit_name" class="form-control" placeholder="Name">
+                            <span id="error_name_msg"></span>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="email">Slug</label>
+                            <input type="text" name="slug" id="edit_slug" class="form-control" placeholder="Slug">
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="mb-3">
+                            <label>Status</label>
+                            <select name="status" id="edit_status" class="form-control">
+                                <option value="Active">Active</option>
+                                <option value="Inactive">Inactive</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="mb-3">
+                            <button type="button" id="add-subCategory" class="btn btn-primary">Update</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
